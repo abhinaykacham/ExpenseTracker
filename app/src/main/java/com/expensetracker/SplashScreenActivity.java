faacc869d77c023ab242e44e6bb4d4d5a8c6f5bc
abhinay <abhinay7777@gmail.com>
2020-07-01 22:10:23 -0700

    Fix: Activity navigates to Home/Login based on session state

--- app/src/main/java/com/expensetracker/SplashScreenActivity.java
+++ app/src/main/java/com/expensetracker/SplashScreenActivity.java
@@ -3,6 +3,7 @@
 package com.expensetracker;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Bundle;
 import android.view.WindowManager;
@@ -12,21 +13,33 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SplashScreenActivity extends AppCompatActivity {
     private DBHelper mydb ;
     private static int SPLASH_SCREEN_TIME_OUT = 800;
-
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mydb = new DBHelper(this);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-//                Intent i = new Intent(SplashScreenActivity.this,
-//                        LoginActivity.class);
-                Intent i = new Intent(SplashScreenActivity.this,
-                        HomeScreenActivity.class);
-                startActivity(i);
-                finish();
-            }
-        }, SPLASH_SCREEN_TIME_OUT);
+        sharedPreferences=this.getSharedPreferences("expensetracker",MODE_PRIVATE);
+        if(sharedPreferences.contains("username")) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(SplashScreenActivity.this,
+                            HomeScreenActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }, SPLASH_SCREEN_TIME_OUT);
+        }else{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(SplashScreenActivity.this,
+                            LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }, SPLASH_SCREEN_TIME_OUT);
+        }
+
     }
 }
